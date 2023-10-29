339adbf15ed295b82ec85de2c36e006079cd0ddb
medoum <mohamedoum2430@gmail.com>
2023-10-29 15:02:45 +0100

    Adding icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import Header from "./component/Header";
 import Tasks from "./component/Tasks";
+import { useState } from "react";
 
 const App = () => {
- 
+  
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: 'Mohamed first Work',
+        day: '23 octobre 2023',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Mohamed Second Work',
+        day: '24 octobre 2023',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Mohamed Third Work',
+        day: '25 octobre 2023',
+        reminder: true,
+      },
+      {
+        id: 4,
+        text: 'Mohamed Last Work',
+        day: '26 octobre 2023',
+        reminder: false,
+      }
+])
   return (
     <div className="container">
       <Header />
-       <Tasks />
+       <Tasks tasks={tasks} />
     </div>
     
   )
--- src/component/Tasks.js
+++ src/component/Tasks.js
@@ -1,9 +1,13 @@
+import Task from "./Task";
 
-const Tasks = () => {
+
+const Tasks = ({tasks}) => {
+    
     return (
+       
       <>
         {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task}/>
         ))}
       </>
     )
